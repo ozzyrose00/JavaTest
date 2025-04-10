fa39e76d4abe89c9f9000b6b755290f72b83b1a6
ozzyrose00 <ozzyrose00@gasystem.co.kr>
2025-04-10 15:18:32 +0900

    234

--- src/test01/TestPrint.java
+++ src/test01/TestPrint.java
@@ -3,5 +3,7 @@ package test01;
 public class TestPrint {
 	public static void main(String[] args) {
 		System.out.println("DDDDDDDDDDDDDDDDDDD");
+		System.out.println("DDDDDDDDDDDDDDDDDDD");
+		System.out.println("DDDDDDDDDDDDDDDDDDD");
 	}
 }
